e29f83bb9685344f81c471986de74760bcd1b740
Gustavo Nogai Saito <gustavo.nogaisaito@usp.br>
2022-03-27 10:12:48 -0300

    Add atacker's and defender's strategies.

--- src/attacker.c
+++ src/attacker.c
@@ -1,5 +1,8 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
 
 // Internal headers
 #include "direction.h"
@@ -16,14 +19,92 @@
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
+bool equal_direction(direction_t d1, direction_t d2){
+
+  return (d1.i == d2.i) && (d1.j == d2.j);
+
+}
+
+direction_t random_direction(direction_t previous_direction){
+
+  direction_t selected_direction = previous_direction;
+
+  srand(time(NULL));
+
+  while (equal_direction(selected_direction, previous_direction)){
+
+    switch (rand() % 5){
+
+      case 0:
+        selected_direction = (direction_t) DIR_RIGHT;
+        break;
+  
+      case 1:
+        selected_direction = (direction_t) DIR_UP_RIGHT;
+        break;  
+
+      case 2:
+        selected_direction = (direction_t) DIR_DOWN_RIGHT;
+        break;  
+
+      case 3:
+        selected_direction = (direction_t) DIR_UP;
+        break;  
+
+      case 4:
+        selected_direction = (direction_t) DIR_DOWN;
+        break;  
+
+      default:
+        printf("Error on attacker function random_direction.\n");
+        break;
+
+    }
+  }
+  return selected_direction;
+}
+
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
+      
   UNUSED(defender_spy);
 
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+  static position_t previous_position = INVALID_POSITION;
+  static direction_t best_direction = (direction_t) DIR_RIGHT;
+
+  if (equal_positions(attacker_position, previous_position)){
+
+    best_direction = random_direction(best_direction);
+
+  }
+
+  else {
+    
+    srand(time(NULL));
+
+    switch (rand() % 3){
+
+    case 0:
+      best_direction = (direction_t) DIR_RIGHT;
+      break;
+    
+    case 1:
+      best_direction = (direction_t) DIR_UP_RIGHT;
+      break;
+
+    case 2:
+      best_direction = (direction_t) DIR_DOWN_RIGHT;
+      break;
+
+    default:
+      printf("Error on attacker 3-way forward randomizer.\n");
+      break;
+
+    }
+  }
+
+  previous_position = attacker_position;
+  return best_direction;
 }
 
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -1,5 +1,8 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
 
 // Internal headers
 #include "direction.h"
@@ -16,14 +19,92 @@
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
+bool equal_direction(direction_t d1, direction_t d2){
+
+  return (d1.i == d2.i) && (d1.j == d2.j);
+
+}
+
+direction_t random_direction(direction_t previous_direction){
+
+  direction_t selected_direction = previous_direction;
+
+  srand(time(NULL));
+
+  while (equal_direction(selected_direction, previous_direction)){
+
+    switch (rand() % 5){
+
+      case 0:
+        selected_direction = (direction_t) DIR_LEFT;
+        break;
+  
+      case 1:
+        selected_direction = (direction_t) DIR_UP_LEFT;
+        break;  
+
+      case 2:
+        selected_direction = (direction_t) DIR_DOWN_LEFT;
+        break;  
+
+      case 3:
+        selected_direction = (direction_t) DIR_UP;
+        break;  
+
+      case 4:
+        selected_direction = (direction_t) DIR_DOWN;
+        break;  
+
+      default:
+        printf("Error on defender function random_direction.\n");
+        break;
+
+    }
+  }
+  return selected_direction;
+}
+
 direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
+      
   UNUSED(attacker_spy);
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+  static position_t previous_position = INVALID_POSITION;
+  static direction_t best_direction = (direction_t) DIR_LEFT;
+
+  if (equal_positions(defender_position, previous_position)){
+
+    best_direction = random_direction(best_direction);
+
+  }
+
+  else {
+    
+    srand(time(NULL));
+
+    switch (rand() % 3){
+
+    case 0:
+      best_direction = (direction_t) DIR_LEFT;
+      break;
+    
+    case 1:
+      best_direction = (direction_t) DIR_UP_LEFT;
+      break;
+
+    case 2:
+      best_direction = (direction_t) DIR_DOWN_LEFT;
+      break;
+
+    default:
+      printf("Error on defender 3-way backward randomizer.\n");
+      break;
+
+    }
+  }
+
+  previous_position = defender_position;
+  return best_direction;
 }
 
 /*----------------------------------------------------------------------------*/
